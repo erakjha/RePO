b2d3d5fbe968c7c76d5ec5531b0244f492b2ba1b
Rakesh Jha <62699584+erakjha@users.noreply.github.com>
2021-11-27 12:34:34 +0530

    Revert "Sunday (#3)" (#4)
    This reverts commit 31344897a8d49bb749fc4d65673f616b072da0b0.

--- erakjha/script.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-sudo hostnamectl set-hostname jenkins.example.com
-sudo yum install docker -y
-echo -e "192.168.77.20 jenkins.example.com" >> /etc/hosts
-sudo systemctl enable docker
-sudo systemctl start docker
-sudo docker pull jenkins/jenkins:lts
-sudo docker run -p 8080:8080 -p 50000:50000 -v jenkins_home:/var/jenkins_home jenkins/jenkins:lts
-
